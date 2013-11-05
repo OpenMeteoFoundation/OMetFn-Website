285902fffb5ef3575f145dc8ee86f6712e5db8b2
Pablo SEMINARIO <pablo@seminar.io>
2013-11-05 09:57:16 +0100

    Replace many latin chars by HTML entities

--- pages/en/support-us.php
+++ pages/en/support-us.php
@@ -1,16 +1,16 @@
 <!-- %TITLE%: "Support us" -->
 <h1>Support us</h1>
 <p>The Open Meteo Foundation is a non-profit organization. All our work is provided openly and for free. Your support helps us continue and expand our activities.</p>
-<p><a href="#why">More about why to support us »</a></p></blockquote>
+<p><a href="#why">More about why to support us &#xbb;</a></p></blockquote>
 <h4>Contribute monthly with your credit card through PayPal.</h4>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_xclick-subscriptions" /><input type="hidden" name="business" value="2PH6MD3M2W45W" /><input type="hidden" name="lc" value="GB" /><input type="hidden" name="no_note" value="1" /><input type="hidden" name="no_shipping" value="2" /><input type="hidden" name="rm" value="1" /><input type="hidden" name="return" value="http://openmeteofoundation.org/thank-you" /><input type="hidden" name="src" value="1" /><input type="hidden" name="bn" value="PP-SubscriptionsBF:btn_subscribeCC_LG.gif:NonHostedGuest" /><input type="hidden" name="item_name" value="Monthly donation to the Open Meteo Foundation" /><input type="hidden" name="currency_code" value="EUR" /><input type="hidden" name="t3" value="M" /><input type="hidden" name="p3" value="1" />
 <select name="a3">
-<option value="3.00">€ 3.00 a month</option>
-<option value="5.00">€ 5.00 a month</option>
-<option selected="selected" value="10.00">€ 10.00 a month</option>
-<option value="20.00">€ 20.00 a month</option>
-<option value="30.00">€ 30.00 a month</option>
-<option value="50.00">€ 50.00 a month</option>
+<option value="3.00">&euro; 3.00 a month</option>
+<option value="5.00">&euro; 5.00 a month</option>
+<option selected="selected" value="10.00">&euro; 10.00 a month</option>
+<option value="20.00">&euro; 20.00 a month</option>
+<option value="30.00">&euro; 30.00 a month</option>
+<option value="50.00">&euro; 50.00 a month</option>
 </select>
 <p>
 <input type="hidden" name="on1" value="Publicly display name?" />
@@ -32,18 +32,18 @@ Have your name publicly displayed on our supporters page.
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_donations" /><input type="hidden" name="business" value="2PH6MD3M2W45W" /><input type="hidden" name="lc" value="GB" /><input type="hidden" name="no_note" value="1" /><input type="hidden" name="no_shipping" value="2" /><input type="hidden" name="return" value="http://openmeteodata.org/index.php/donate/thank-you/" /><input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted" /><input type="hidden" name="item_name" value="Donation to the Open Meteo Foundation" /><input type="hidden" name="currency_code" value="EUR" />
 <p>
 <select name="amount">
-<option value="2.00">€ 2.00</option>
-<option value="5.00">€ 5.00</option>
-<option selected="selected" value="10.00">€ 10.00</option>
-<option value="20.00">€ 20.00</option>
-<option value="50.00">€ 50.00</option>
-<option value="100.00">€ 100.00</option>
+<option value="2.00">&euro; 2.00</option>
+<option value="5.00">&euro; 5.00</option>
+<option selected="selected" value="10.00">&euro; 10.00</option>
+<option value="20.00">&euro; 20.00</option>
+<option value="50.00">&euro; 50.00</option>
+<option value="100.00">&euro; 100.00</option>
 </select>
 </p>
 <p><input type="submit" value="Donate" /></p>
 <p><span style="font-size: 80%;"><input type="hidden" name="on3" value="Fiscal receipt?" /><label><input type="checkbox" name="os3" value="French citizen" /> I&#8217;m a French citizen, I want a fiscal receipt for up to 66% tax refund.</label><br><label><input type="checkbox" name="os3" value="French company" /> I&#8217;m a French company, I want a fiscal receipt for up to 60% tax refund.</label><!--<a title="Tax refunds" href="http://openmeteodata.org/index.php/donate/tax-refunds/" target="_blank">Learn more about tax refunds&#8230;</a>--></span></p>
 </form>
-<p><small>Your credit card donation will be processed by PayPal. The charge will appear as “Open Meteo Foundation” on your credit card statement.</small></p>
+<p><small>Your credit card donation will be processed by PayPal. The charge will appear as &#8220;Open Meteo Foundation&#8221; on your credit card statement.</small></p>
 <h5>Wire transfer</h5>
 <p>
 Account Holder: OpenMeteoData<br />
@@ -61,4 +61,4 @@ We are setting-up web servers, operating High-Performance-Computing cluster for 
 <p><strong>Your support is therefore crucial to us in continuing and expanding our work.</strong></p>
 <p>If you would like to know more about our activities please take a look at our <a href="/projects" title="Projects">projects</a> page. You&#8217;ll find our most recent annual reports and our annual accounts on our <a href="/governance" title="Governance" target="_blank">governance</a> page. </p>
 <h3>Other ways to support the Open Meteo Foundation</h3>
-<p>You can support the Open Meteo Foundation in many other ways than via a financial contribution. Much of what we do depends on those <strong>who volunteer their time and effort</strong> — be that acting as a board member, helping to develop and maintain our websites, organizing events, and much more. <strong>Why not become one of them?</strong> If you would like to get involved please visit the <a title="Get involved !" href="/get-involved">get Involved</a> page.</p>
+<p>You can support the Open Meteo Foundation in many other ways than via a financial contribution. Much of what we do depends on those <strong>who volunteer their time and effort</strong> &#8211; be that acting as a board member, helping to develop and maintain our websites, organizing events, and much more. <strong>Why not become one of them?</strong> If you would like to get involved please visit the <a title="Get involved !" href="/get-involved">get Involved</a> page.</p>
